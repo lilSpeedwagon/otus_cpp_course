b9e5d81e4add27113ebf75558c32476fc0636e65
Speedwagon <ded_eg0r@mail.ru>
2022-10-10 15:56:22 +0300

    test files

---
+++ task_2_ip_filter/tests/test_utils.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE test_utils
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_utils)
+
+BOOST_AUTO_TEST_CASE(test_valid_utils) {
+	BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
