68163f0ecaa5d144430a122d19420cdd26be5215
lilSpeedwagon <ded_eg0r@mail.ru>
2022-11-01 00:44:26 +0400

    block allocator

--- task_3_allocator/src/block_allocator.hpp
+++ task_3_allocator/src/block_allocator.hpp
@@ -3,35 +3,40 @@
 #include <memory>
 #include <bitset>
 
+#include <iostream> // TODO remove
+
 namespace allocators {
 
 template<typename T, size_t BlockSize>
-class BlockAllocator : std::allocator_traits<BlockAllocator> {
+class BlockAllocator {
 public:
     using value_type = T;
+    using BlockBitmap = std::bitset<BlockSize>;
 
     static constexpr const size_t block_size = BlockSize;
 
     // sequental block of memory with fixed size of T
     struct MemoryBlock {
         // marks non-empty cells
-        BlockBitmap free_flags_;
+        BlockBitmap free_flags;
         // data in block 
-        T buffer_[block_size];
+        T buffer[block_size];
         // pointer to the next MemoryBlock
         MemoryBlock* next_ptr;
 
         // Returns pointer to the available segment of memory of size n,
         // nullptr if no such segment is available.
-        T* find_free_segment(size_t n) {
-            if (free_flags_.none()) {
+        T* acquire_free_segment(size_t n) {
+            if (free_flags.all()) {
                 return nullptr;
             }
             size_t free_sequence_size = 0;
-            for (size_t i = 0; i < free_flags_.size(); i++) {
-                if (!free_flags_[i]) {
+            for (size_t i = 0; i < free_flags.size(); i++) {
+                if (!free_flags[i]) {
                     if (++free_sequence_size == n) {
-                        return buffer_ + i - (n - 1);
+                        size_t from = i - n + 1;
+                        set_flags(from, n, true);
+                        return buffer + from;
                     }
                 } else {
                     free_sequence_size = 0;
@@ -39,53 +44,137 @@ public:
             }
             return nullptr;
         }
-    };
 
-    BlockAllocator() noexcept : blocks_allocated_{0}, size_{0}, blocks_list_head_{nullptr} {}
-    ~BlockAllocator() {}
-
-    T* allocate (std::size_t n) {
-        if (n > block_size) {
-            throw std::bad_alloc();
+        void release_segment(T* ptr, size_t n) {
+            set_flags(ptr - buffer, n, false);
         }
 
-        if (size_ + n >= blocks_allocated_ * block_size) {
-            return allocate_memory_block()->buffer;
-        }
-
-        auto block_ptr = blocks_list_head_;
-        while (block_ptr != nullptr) {
-            if (auto segment_ptr = block_ptr->get_free_segment_of_size(n);
-                segment_ptr != nullptr) {
-                return segment_ptr;
+        void set_flags(size_t from, size_t count, bool value) {
+            for (size_t i = from; i < from + count; i++) {
+                free_flags[i] = value;
             }
         }
 
-        return allocate_memory_block()->buffer;
+        bool has_memory_segment(T* ptr, size_t n) {
+            auto buffer_ptr = static_cast<T*>(buffer);
+            return ptr >= buffer_ptr && ptr + n <= buffer_ptr + block_size;
+        }
+
+        bool empty() {
+            return free_flags.none();
+        }
+    };
+
+    BlockAllocator() noexcept 
+        : blocks_allocated_{0}, size_{0}, blocks_list_head_{nullptr} {}
+    
+    ~BlockAllocator() {
+        clear();
     }
 
-    void deallocate (T* p, std::size_t n) {
-        // how to find MemoryBlock?
-        // sequental lookup and address math ???
-        // free memory, mark memory block segment as empty
-        // if memory block is empty - deallocate block
+    T* allocate (std::size_t n) {
+        std::cout << "allocate(" << n << ")\n";
+        if (n > block_size) {
+            throw std::bad_alloc();
+        }
+        size_ += n;
+        std::cout << "new size " << size_ << '\n';
+        if (size_ > blocks_allocated_ * block_size) {
+            std::cout << "it is not enough free memory, allocate new block\n";
+            allocate_memory_block();
+        }
+        if (auto free_segment_ptr = find_free_segment(n);
+            free_segment_ptr != nullptr) {
+            std::cout << "free segment found\n";
+            return free_segment_ptr;
+        }
+        std::cout << "free segment with enough size not found, allocate a new one\n";
+        allocate_memory_block();
+        return find_free_segment(n);
+    }
+
+    void deallocate(T* ptr, std::size_t n) {
+        std::cout << "deallocate " << n << '\n';
+        MemoryBlock* block_ptr = blocks_list_head_;
+        MemoryBlock* prev_ptr = nullptr;
+        while (block_ptr != nullptr) {
+            if (block_ptr->has_memory_segment(ptr, n)) {
+                block_ptr->release_segment(ptr, n);
+                if (block_ptr->empty()) {
+                    if (prev_ptr == nullptr && block_ptr == blocks_list_head_) {
+                        blocks_list_head_ = nullptr;
+                    } else {
+                        prev_ptr->next_ptr = block_ptr->next_ptr;
+                    }
+                    delete block_ptr;
+                    blocks_allocated_--;
+                }
+                if (n > size_) {
+                    size_ = 0;
+                } else {
+                    size_ -= n;
+                }
+                return;
+            }
+            prev_ptr = block_ptr;
+            block_ptr = block_ptr->next_ptr;
+        }
+        throw std::logic_error("memory is corrupted or allocated by other allocator");
+    }
+
+    void clear() {
+        auto block_ptr = blocks_list_head_;
+        while (block_ptr != nullptr) {
+            auto to_delete = block_ptr;
+            block_ptr = block_ptr->next_ptr;
+            delete to_delete;
+        }
+        blocks_allocated_ = 0;
+        size_ = 0;
+        blocks_list_head_ = nullptr;
+    }
+
+    void print() {
+        std::cout << "size " << size_ << '/' << blocks_allocated_ * BlockSize << '\n';
+        auto block_ptr = blocks_list_head_;
+        for (int i = 0; block_ptr != nullptr; i++) {
+            std::cout << "Block " << i << ": " << block_ptr->free_flags;
+            block_ptr = block_ptr->next_ptr;
+        }
+        std::cout << std::endl;
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+    size_t blocks_allocated() const {
+        return blocks_allocated_;
     }
 
 private:
-    using BlockBitmap = std::bitset<BlockSize>;
     static constexpr const size_t real_block_size = sizeof(MemoryBlock);
 
     MemoryBlock* allocate_memory_block() {
         auto block_ptr = new MemoryBlock{};
         block_ptr->next_ptr = blocks_list_head_;
         blocks_list_head_ = block_ptr;
+        blocks_allocated_++;
         return block_ptr;
     }
 
-    MemoryBlock* find_free_block(size_t n) {
+    void deallocate_memory_block(MemoryBlock* block) {
+        delete block;
+        blocks_allocated_--;
+    }
+
+    T* find_free_segment(size_t n) {
         auto current_block_ptr = blocks_list_head_;
         while (current_block_ptr != nullptr) {
-            if (current_block_ptr->find_free_segment)
+            if (auto ptr = current_block_ptr->acquire_free_segment(n); ptr != nullptr) {
+                return ptr;
+            }
+            current_block_ptr = current_block_ptr->next_ptr;
         }
         return nullptr;
     }
@@ -94,5 +183,8 @@ private:
     size_t size_;
     MemoryBlock* blocks_list_head_;
 };
+
+template<typename T>
+class BlockAllocator<T, 0> {};
 
 } // allocators
--- task_3_allocator/src/main.cpp
+++ task_3_allocator/src/main.cpp
@@ -6,7 +6,14 @@
 #include <block_allocator.hpp>
 
 int main() {
-    allocators::BlockAllocator<32> allocator;
+    allocators::BlockAllocator<int, 4> allocator;
+    allocator.print();
 
+    auto ptr = allocator.allocate(4);
+    allocator.print();
+    allocator.deallocate(ptr, 4);
+    allocator.print();
+
+    std::cout << std::endl;
     return 0;
 }
