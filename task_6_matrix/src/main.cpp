da52f82c6a579459fae75f24dc6b10ab4d3de765
lilSpeedwagon <ded_eg0r@mail.ru>
2023-01-09 00:44:46 +0400

    housekeeping

--- task_6_matrix/src/main.cpp
+++ task_6_matrix/src/main.cpp
@@ -4,21 +4,23 @@
 
 
 int main() {
-    containers::InfiniteMatrix<int, -1, 2> matrix;
-    
-    std::cout << matrix[0][0] << '\n';
-    std::cout << "size " << matrix.size() << '\n';
-    matrix[0][0] = 1;
-    std::cout << matrix[0][0] << '\n';
-    std::cout << "size " << matrix.size() << '\n';
-    matrix[1][1] = 2;
-    std::cout << matrix[1][1] << '\n';
+    containers::InfiniteMatrix<int, 0, 2> matrix;
+    for (int i = 0; i < 10; i++) {
+        matrix[i][i] = i;
+        matrix[i][9 - i] = 9 - i;
+    }
 
-    for (const auto& [idx, val] : matrix) {
-        for (const auto& i : idx) {
-            std::cout << i << ' ';
+    for (int i = 1; i <= 8; i++) {
+        for (int j = 1; j <= 8; j++) {
+            std::cout << matrix[i][j] << ' ';
         }
-        std::cout << val << '\n';
+        std::cout << '\n';
+    }
+
+    std::cout << "size " << matrix.size() << '\n';
+
+    for (const auto& [index, val] : matrix) {
+        std::cout << '(' << index[0] << ';' << index[1] << ')' << ' ' << val << '\n';
     }
 
     return 0;
