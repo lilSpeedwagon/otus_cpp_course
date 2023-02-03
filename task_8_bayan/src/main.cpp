15dc049f99ebf826988dad62b35a4a6bdad2ea18
lilSpeedwagon <ded_eg0r@mail.ru>
2023-02-04 00:11:48 +0400

    scan current dir

--- task_8_bayan/src/main.cpp
+++ task_8_bayan/src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+#include <file_scanner/file_scanner.hpp>
+
 
 int main() {
+
+    std::vector<boost::filesystem::path> include = {
+        "./",
+    };
+    std::vector<boost::filesystem::path> exclude = {};
+    std::vector<std::string> wildcards = {};
+
+    bayan::file::FileScanner scanner(include, exclude, 10, 0, wildcards);
+    scanner.Scan();
+    auto files = scanner.GetScannedFiles();
+    std::cout << "files:\n";
+    for (const auto& f : files) {
+        std::cout << f << '\n';
+    }
+
     return 0;
 }
