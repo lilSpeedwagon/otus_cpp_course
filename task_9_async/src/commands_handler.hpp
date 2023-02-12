6f0fc2b12197c80fd3d3d256eac1ec51bc5168da
lilSpeedwagon <ded_eg0r@mail.ru>
2023-02-12 12:52:23 +0400

    init repo with task 7

---
+++ task_9_async/src/commands_handler.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <memory>
+#include <string>
+
+#include <containers/bulk_container.hpp>
+
+
+namespace bulk {
+
+/// @brief Bulk commands interpeteur.
+/// Operates with commands blocks of static and dynamic sized.
+class CommandsHandler {
+public:
+    using command_type = std::string;
+
+    CommandsHandler(
+        std::istream& input_stream,
+        std::ostream& output_stream,
+        std::shared_ptr<BulkContainer<command_type>> commands_contaier,
+        std::shared_ptr<BulkContainer<command_type>> block_contaier);
+    ~CommandsHandler();
+
+    /// @brief Runs commands I/O loop for bulk handling.
+    void Run();
+
+private:
+    static const std::string kBlockBegin;
+    static const std::string kBlockEnd;
+
+    bool IsDynamicBlock() const;
+
+    std::shared_ptr<BulkContainer<command_type>> GetActiveContainer() const;
+
+    void FlushCommands();
+
+    void WriteCommands(const std::vector<command_type>& commands) const;
+
+    size_t dynamic_blocks_opened_;
+    std::istream& input_stream_;
+    std::ostream& output_stream_;
+    std::shared_ptr<BulkContainer<command_type>> commands_contaier_;
+    std::shared_ptr<BulkContainer<command_type>> block_contaier_;
+};
+
+} // namespace bulk
---
+++ task_9_async/src/containers/static_bulk_container.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <exception>
+#include <vector>
+
+#include <containers/bulk_container.hpp>
+
+
+namespace bulk {
+
+template <typename T>
+class StaticBulkContainer : public BulkContainer<T>{
+public:
+    StaticBulkContainer(size_t bulk_size) 
+        : bulk_size_(bulk_size) {
+        Init();
+    }
+    ~StaticBulkContainer() {}
+
+    bool Add(const T& item) {
+        if (IsFull()) {
+            throw std::runtime_error("bulk size exceeded");
+        }
+        container_.push_back(item);
+        return !IsFull();
+    }
+
+    void Clear() {
+        container_.clear();
+        Init();
+    }
+
+    std::vector<T> GetItems() const {
+        return container_;
+    }
+
+    size_t Size() const {
+        return container_.size();
+    }
+
+    size_t BulkSize() const {
+        return bulk_size_;
+    }
+
+    bool IsFull() const {
+        return container_.size() >= bulk_size_;
+    }
+
+private:
+    void Init() {
+        container_.reserve(bulk_size_);
+    }
+
+    size_t bulk_size_;
+    std::vector<T> container_;
+};
+
+} // namespace bulk
